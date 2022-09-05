831900a8a5c6ffd9c255809868089e0cbeb259fb
Salil Patel <101462890+salilpatel@users.noreply.github.com>
2022-09-05 13:35:43 +0000

    problem1

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
-const express = require('express')
+const express = require('express');
 
 const app = express();
+const bp = require('body-parser');
+app.use(bp.json());
 const port = process.env.PORT || 5000;
 
+const addDetails = require('./Problem1/addStudentDetails');
+app.use('/student', addDetails);
+const getDetails = require('./Problem1/getStudentDetails');
+app.use('/student', getDetails);
 
 app.listen(port, () => {
-  console.log(`Server running on port ${port}`)
+	console.log(`Server running on port ${port}`);
 });
